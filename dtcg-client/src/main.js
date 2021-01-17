0b404f3f9cee7635416866b6ed23ae947ef25a26
Leonardo Silveira <sombriks@gmail.com>
2021-01-17 18:12:52 -0300

    add phaser

--- dtcg-client/src/main.js
+++ dtcg-client/src/main.js
@@ -1,4 +1,5 @@
 import Soc from "socket.io-client";
+import Phaser from "phaser";
 
 const io = Soc(process.env.API_URL || "http://localhost:3000");
 console.log(io);
@@ -8,3 +9,15 @@ setTimeout(() => {
 }, 1000);
 
 console.log("teste2");
+
+const game = new Phaser.Game({
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+    preload: function () {},
+    create: function () {},
+    update: function () {},
+  },
+  disableContextMenu: true,
+});
